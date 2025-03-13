82caf79eaecf9177c61faf916492d7b4bf4e1b28
erkebulanmyrzabek <erkemyrzaa@gmail.com>
2025-03-13 14:45:31 +0500

    2

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users.views import telegram_auth
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('hackathon.urls')),
     path('api/users/', include('users.urls')),
+    path('api/auth/telegram/', telegram_auth, name='telegram-auth-alt'),  # Альтернативный путь
 ]
 
 if settings.DEBUG:
